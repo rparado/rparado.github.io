78aa6027ae84c21c9df3a322282a84f33f6b227d
Rolando Parado <rolando.c.parado@accenture.com>
2016-03-12 13:54:51 +0800

    projects ection updates

--- js/script.js
+++ js/script.js
@@ -17,18 +17,32 @@
 		});
 
 		$(window).scroll(function () {
-
-			//Display or hide scroll to top button 
-			if ($(this).scrollTop() > 100) {
-			    $('.scrollup').fadeIn();
+			if ($(this).scrollTop() > 50) {
+			    $('.navbar').addClass('sticky animated fadeInDown');
+			     $('.scrollup').fadeIn();
 			} else {
+			    $('.navbar').removeClass('sticky animated fadeInDown');
 			    $('.scrollup').fadeOut();
 			}
 
-			if ($(this).scrollTop() > 50) {
-			    $('.navbar').addClass('animated fadeInDown');
-			} else {
-			    $('.navbar').removeClass('animated fadeInDown');
+			var fromTop = $(this).scrollTop() + topMenuHeight + 10;
+
+			// Get id of current scroll item
+			var cur = scrollItems.map(function () {
+			if ($(this).offset().top < fromTop)
+				return this;
+			});
+
+			// Get the id of the current element
+			cur = cur[cur.length - 1];
+			var id = cur && cur.length ? cur[0].id : "";
+
+			if (lastId !== id) {
+				lastId = id;
+				// Set/remove active class
+				menuItems
+				.parent().removeClass("active")
+				.end().filter("[href=#" + id + "]").parent().addClass("active");
 			}
 		});
 
@@ -42,5 +56,23 @@
 	        }
 	    });
 
+		$('#project-grid').mixitup();
+
+		/*scroll to top*/
+		$('.scrollup').click(function () {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+
+		/*animate thumbnail*/
+		$('.thumbnail').one('inview', function (event, visible) {
+			if (visible == true) {
+				$(this).addClass("animated fadeInDown");
+			} else {
+				$(this).removeClass("animated fadeInDown");
+			}
+		});
 	});
 })(jQuery);
